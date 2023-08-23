f7db2b4bccb048038d0b7bf0b3fec7205f7f9cdd
Nidhinsha <nidhinsha64@gmail.com>
2023-08-23 22:24:25 +0530

    first commit

---
+++ app/api/prompt/route.js
@@ -0,0 +1,20 @@
+import Prompt from "@models/prompt"
+import { connectToDB } from "@utils/database"
+
+export const GET = async (req) => {
+    try {
+        await connectToDB()
+
+        const prompts = await Prompt.find({}).populate('creator')
+
+        return new Response(JSON.stringify(prompts), {
+            status: 200
+        })
+    } catch (error) {
+        console.log(error);
+
+        return new Response("Error while finding the prompts", {
+            status: 500
+        })
+    }
+}
---
+++ components/Prompt.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Prompt = () => {
+  return (
+    <div>
+      propmts
+    </div>
+  )
+}
+
+export default Prompt
